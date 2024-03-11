f33a4fc3bc6c069fa3a43edb5207e4d51d4d869d
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2024-03-11 19:25:13 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularfirst';
+  pageTitle:String="Angular Data Binding Practice";
+  a:number=2;
+  b:number=2;
+  sum: number = this.a + this.b;
+  imageUrl:string="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj";
+  disable=false;
+  message:string="Button is Clicked"
+  username:string=""
+  
+  toggleChange(){
+    this.disable = !this.disable;
+  }
+  handleButtonClick(message:string){
+    console.log(message);
+  }
 }
