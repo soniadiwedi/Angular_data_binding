7852d3cb2e0326fe9f668830a26c46c9dd095cc9
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2024-03-11 19:28:31 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent {
   imageUrl:string="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj";
   disable=false;
   message:string="Button is Clicked"
-  username:string=""
+  username:string="";
+  isError: boolean = true;
   
   toggleChange(){
     this.disable = !this.disable;
